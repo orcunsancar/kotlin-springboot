4cb75ebf3e45ce627ab6a2b54260863a9f4bb57b
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-08-31 15:53:20 +0300

    val and var variables in kotlin

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,14 @@
+package com.kotlinplayground.basics
+
+fun main() {
+
+    val name: String = "Dilip"
+    println(name)
+
+    // name = "Dilip1"
+
+    var age: Int = 34
+    println(age)
+    age = 35
+    println(age)
+}
