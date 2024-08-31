9da4d0d5595f2d54d463845b890deccbc4a793c4
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-08-31 18:55:39 +0300

    top-level functions and top-level properties

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -1,5 +1,8 @@
 package com.kotlinplayground.basics
 
+import com.kotlinplayground.functions.courseName
+import com.kotlinplayground.functions.topLevelFunction
+
 fun main() {
 
     val name = "Dilip"
@@ -26,4 +29,8 @@ fun main() {
         DEF
     """.trimIndent()
     println(multiLine1)
+
+    val num = topLevelFunction()
+    println("Num is : $num")
+    println("courseName : $courseName")
 }
