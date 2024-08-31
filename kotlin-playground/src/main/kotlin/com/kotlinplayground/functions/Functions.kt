726163f09efe0a0d1e40382d5089a0011268c448
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-08-31 18:49:24 +0300

    default value parameters and named arguments

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/functions/Functions.kt
@@ -1,5 +1,7 @@
 package com.kotlinplayground.functions
 
+import java.time.LocalDate
+
 fun printName(name: String) {
     println("Name is : $name")
 }
@@ -9,13 +11,25 @@ fun addition(x: Int, y: Int): Int {
 }
 fun addition_approach1(x: Int, y: Int): Int = x + y
 
+fun printPersonDetails(
+    name: String,
+    email: String = "",
+    dob: LocalDate = LocalDate.now()) {
+    println("Nameis $name and email is $email and the dob is $dob")
+}
 
 fun main() {
     printName("Dilip")
     val result = addition(1, 2)
     println("Result is : $result")
+
     val result1 = addition_approach1(1, 2)
     println("Result is : $result1")
+
+    printPersonDetails("Dilip", "abc@gmail.com", LocalDate.parse("2000-01-01"))
+    printPersonDetails("Dilip")
+    printPersonDetails(name = "Dilip", dob = LocalDate.parse("2000-01-01"))
+    printPersonDetails( dob = LocalDate.parse("2000-01-01"), name = "Dilip", email = "abc@gmail.com")
 }
 
 
