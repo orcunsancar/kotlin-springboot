24b2aaf4e74511d4a0593eb3deb8f9b35c4b3321
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-08-31 15:59:18 +0300

    basic types in kotlin

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -2,13 +2,28 @@ package com.kotlinplayground.basics
 
 fun main() {
 
-    val name: String = "Dilip"
+    val name = "Dilip"
     println(name)
 
     // name = "Dilip1"
 
-    var age: Int = 34
+    var age = 34
     println(age)
     age = 35
     println(age)
+
+    val salary = 30000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    val multiLine = "ABC \n DEF"
+    println(multiLine)
+
+    val multiLine1 = """
+        ABC
+        DEF
+    """.trimIndent()
+    println(multiLine1)
 }
