2c4dd325f48403b6ebe097c75d571054f5862ad7
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-01 16:18:51 +0300

    secondary constructors

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -4,6 +4,14 @@ class Person(
     val name: String = "",
     val age: Int = 0
 ) {
+    var email: String = ""
+    constructor(
+        _email: String,
+        _name: String = "",
+        _age: Int = 0
+    ) : this(_name, _age) {
+        email = _email
+    }
 
     fun action() {
         println("Person Walks")
@@ -17,4 +25,8 @@ fun main() {
 
     val person1 = Person()
     println("Name : ${person1.name}  and the age is ${person1.age}")
+
+    val person2 = Person(_email = "abc@gmail.com","Alex", 25)
+    println("Name : ${person2.name}  and the age is ${person2.age} amd the email is " +
+            "${person2.email}")
 }
