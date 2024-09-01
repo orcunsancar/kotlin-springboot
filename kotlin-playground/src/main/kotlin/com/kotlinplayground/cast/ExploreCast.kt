ef3379cca820de3f51294d548ba6c447b6b0481d
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-01 19:00:36 +0300

    type checking and casting in kotlin

---
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/cast/ExploreCast.kt
@@ -0,0 +1,38 @@
+package com.kotlinplayground.cast
+
+import com.kotlinplayground.classes.Course
+
+fun checkType(type: Any) {
+
+    when (type) {
+        is Course -> {
+            // val course = type as Course
+            println(type.copy())
+        }
+        is String -> {
+            println(type.lowercase())
+        }
+    }
+}
+fun castNumber(any: Any) {
+    when (any) {
+        any as Double -> println("Value is Double")
+    }
+}
+
+fun main() {
+
+    val course = Course(1,
+        "Reactive Programming in Modern Java using Project Reactor",
+        "Dilip")
+
+    checkType(course)
+    checkType("DILIP")
+
+    castNumber(1.0)
+    // castNumber(1)
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+}
