f3b7538fd18ab71a5bf424917d6009239f9707f3
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-01 16:10:50 +0300

    primary constructors

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -1,6 +1,9 @@
 package com.kotlinplayground.classes
 
-class Person {
+class Person(
+    val name: String = "",
+    val age: Int = 0
+) {
 
     fun action() {
         println("Person Walks")
@@ -8,6 +11,10 @@ class Person {
 }
 
 fun main() {
-    val person = Person()
+    val person = Person("Alex", 25)
     person.action()
+    println("Name : ${person.name}  and the age is ${person.age}")
+
+    val person1 = Person()
+    println("Name : ${person1.name}  and the age is ${person1.age}")
 }
