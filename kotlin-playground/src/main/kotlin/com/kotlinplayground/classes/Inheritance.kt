b9ba92c2433209864a7af71dc6844d76eb460ba3
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-01 18:18:12 +0300

    override functions and variables

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Inheritance.kt
@@ -1,13 +1,19 @@
 package com.kotlinplayground.classes
 
 open class User(val name: String) {
-
-    fun login() {
+    open var isLoggedIn: Boolean = false
+    open fun login() {
         println("Inside user Login")
     }
 }
 
-class Student(name: String) : User(name)
+class Student(name: String) : User(name) {
+    override var isLoggedIn: Boolean = false
+    override fun login() {
+        println("Inside Student Login")
+        super.login()
+    }
+}
 
 class Instructor(name: String) : User(name)
 
@@ -16,6 +22,8 @@ fun main() {
     val student = Student("Alex")
     println("name is: ${student.name}")
     student.login()
+    student.isLoggedIn = true
+    println("Logged in values is: ${student.isLoggedIn}")
 
     val instructor = Instructor("Dilip")
     println("name is: ${instructor.name}")
