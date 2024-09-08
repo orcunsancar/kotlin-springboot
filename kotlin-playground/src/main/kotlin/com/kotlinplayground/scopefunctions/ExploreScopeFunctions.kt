a0a217e3ae992760d4750a555100c4f77625e7e7
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-08 16:30:38 +0300

    with and run scope functions

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
@@ -7,7 +7,43 @@ fun main() {
 
    // exploreApply()
    // exploreAlso()
-    exploreLet()
+   //  exploreLet()
+    // exploreWith()
+    exploreRun()
+}
+
+fun exploreRun() {
+
+    var numbers : MutableList<Int>? = null
+    val result = numbers.run {
+
+        numbers = mutableListOf(1, 2, 3, 4, 5)
+        numbers?.sum()
+    }
+
+    println("Run Result is : $result")
+
+    val length = run {
+        val name = "Dilip"
+        println(name)
+        name.length
+    }
+
+    println("Run length is $length")
+}
+
+fun exploreWith() {
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+
+    val result = with(numbers) {
+        /*println("Size is ${numbers.size}")
+        val list = numbers.plus(6)
+        list.sum()*/
+        println("Size is $size")
+        sum()
+    }
+
+    println("With Result is : $result")
 }
 
 fun exploreLet() {
