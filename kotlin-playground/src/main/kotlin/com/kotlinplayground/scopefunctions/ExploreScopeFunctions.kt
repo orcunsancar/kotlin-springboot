5d37c38f5858d3d247bfa361f86cba4a008feb9d
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-08 16:21:25 +0300

    let scope function

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
@@ -5,8 +5,27 @@ import com.kotlinplayground.classes.CourseCategory
 
 fun main() {
 
-    exploreApply()
-    exploreAlso()
+   // exploreApply()
+   // exploreAlso()
+    exploreLet()
+}
+
+fun exploreLet() {
+
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+    val result = numbers.map { it * 2 }.filter { it > 5 }. let {
+        println(it)
+        it.sum()
+    }
+    println(result)
+
+    var name : String? = null
+    name = "Dilip"
+    val result1 = name?.let {
+        println(it)
+        it.uppercase()
+    }
+    println(result1)
 }
 
 fun exploreApply() {
