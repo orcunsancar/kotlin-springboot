49c7f792d55a70ab5698b0501a234478665fbaf2
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-15 16:07:18 +0300

    create CourseReposiştory and configure jpa in yaml

---
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/course_catalog_service/repository/CourseRepository.kt
@@ -0,0 +1,7 @@
+package com.kotlinspring.course_catalog_service.repository
+
+import com.kotlinspring.course_catalog_service.entity.Course
+import org.springframework.data.repository.CrudRepository
+
+interface CourseRepository : CrudRepository<Course, Int> {
+}
