96b3111dc031a265c4ed0aaeb38b03c932c8d92d
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-15 16:27:29 +0300

    integration test added for addCourse

---
+++ course-catalog-service/src/test/intg/com.kotlinspring.course_catalog_service.controller/CourseControllerIntgTest.kt
@@ -0,0 +1,40 @@
+package com.kotlinspring.course_catalog_service.controller
+
+import com.kotlinspring.course_catalog_service.dto.CourseDTO
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+
+        val courseDTO = CourseDTO(null, "Build Restful APIs using Kotlin and SpringBoot",
+            "Development")
+
+        val savedCourseDTO = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDTO!!.id != null
+        }
+    }
+}
