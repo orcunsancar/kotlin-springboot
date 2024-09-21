c4d5eb146184c1799d3f7cdf41103519f3dac4fd
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-21 17:32:37 +0300

    testing multiple sets of data using @ParameterizedTest

--- course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/repository/CourseRepositoryIntgTest.kt
+++ course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/repository/CourseRepositoryIntgTest.kt
@@ -4,9 +4,13 @@ import com.kotlinspring.course_catalog_service.util.courseEntityList
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -24,7 +28,7 @@ class CourseRepositoryIntgTest {
 
     }
     @Test
-    fun findByNameContaining() {
+    fun findCoursesByNameContaining() {
 
         val courses = courseRepository.findByNameContaining("SpringBoot")
         println("courses: $courses")
@@ -33,11 +37,28 @@ class CourseRepositoryIntgTest {
     }
 
     @Test
-    fun findByName() {
+    fun findCoursesByName() {
 
         val courses = courseRepository.findCoursesByName("SpringBoot")
         println("courses: $courses")
 
         assertEquals(2, courses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+        val courses = courseRepository.findCoursesByName(name)
+
+        println("courses  : $courses")
+        assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1))
+        }
     }
 }
