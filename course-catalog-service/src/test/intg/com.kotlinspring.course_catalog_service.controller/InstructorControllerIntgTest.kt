830b6d55fcae29b2b6079e1621029209a4366ade
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2024-09-21 19:03:54 +0300

    integration test added for createInstructor service

---
+++ course-catalog-service/src/test/intg/com.kotlinspring.course_catalog_service.controller/InstructorControllerIntgTest.kt
@@ -0,0 +1,41 @@
+package com.kotlinspring.course_catalog_service.controller
+
+import com.kotlinspring.course_catalog_service.dto.InstructorDTO
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class InstructorControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun createInstructor() {
+
+        val instructorDTO =  InstructorDTO(null, "Dilip Sundarraj")
+
+        val savedInstructorDTO = webTestClient
+            .post()
+            .uri("/v1/instructors")
+            .bodyValue(instructorDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(InstructorDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedInstructorDTO!!.id != null
+        }
+    }
+
+
+}
